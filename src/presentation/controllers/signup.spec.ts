8ec54f08011c15b8302672bf67c882489579f653
Mairon Vilela <maironvilela@hotmail.com>
2022-01-07 12:32:40 -0300

    test: ensure that the controller returns code 400 if the username is not provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('Signup', () => {
+  test('should be able return code 400 if the username is not provide', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      email: 'any_email@emnail.com',
+      password: 'any_password',
+      passwordConfirmation: 'any_password'
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toEqual(400)
+  })
+})
