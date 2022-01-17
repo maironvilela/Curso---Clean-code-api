a532435ecf32153537b6494db5ed8fa03e030c78
Mairon Vilela <maironvilela@hotmail.com>
2022-01-17 16:28:08 -0300

    feat: ensure that the LoginController returns code 500 in case of failure to validate the email

--- src/presentation/controllers/login/index.ts
+++ src/presentation/controllers/login/index.ts
@@ -1,33 +1,41 @@
 import { InvalidParamError, MissingParamError } from '../../error';
-import { badRequest, ok } from '../../helpers/http-helpers';
+import {
+  badRequest,
+  internalServerError,
+  ok,
+} from '../../helpers/http-helpers';
 import { HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
 export class LoginController {
   constructor(private readonly emailValidator: EmailValidator) {}
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = request.body;
+    try {
+      const { email, password } = request.body;
 
-    if (!email) {
-      return await new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('email'))),
-      );
+      if (!email) {
+        return await new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('email'))),
+        );
+      }
+
+      if (!password) {
+        return await new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('password'))),
+        );
+      }
+
+      const isValidEmail = this.emailValidator.validate(email);
+
+      if (!isValidEmail) {
+        return await new Promise(resolve =>
+          resolve(badRequest(new InvalidParamError('email'))),
+        );
+      }
+
+      return await new Promise(resolve => resolve(ok({})));
+    } catch (err) {
+      return internalServerError(err);
     }
-
-    if (!password) {
-      return await new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('password'))),
-      );
-    }
-
-    const isValidEmail = this.emailValidator.validate(email);
-
-    if (!isValidEmail) {
-      return await new Promise(resolve =>
-        resolve(badRequest(new InvalidParamError('email'))),
-      );
-    }
-
-    return await new Promise(resolve => resolve(ok({})));
   }
 }
